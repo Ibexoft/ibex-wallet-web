cebe1dbd6173ad64c11b39683cfdf9fb61de7e9b
Hamza Bham <92208535+MHamzaBham@users.noreply.github.com>
2024-08-12 21:21:06 +0500

    XP-66 feat: Accounts UI

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,18 @@ import './assets/css/style.css'
 // import './assets/js/main.js'
 import Header from './components/Header.jsx'
 import Home from './pages/Home.jsx'
+import Account from './pages/Accounts.jsx'
 
 function App() {
 
 
   return (
     <>
-      <Header />
-      <Home />
+      <main id="main" className="main">
+        <Header />
+        <Home />
+        {/* <Account /> */}
+      </main>
     </>
   )
 }
