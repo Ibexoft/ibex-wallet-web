c231823c2c8a87ab781c704de6d141035d1a17fc
Hamza Bham <92208535+MHamzaBham@users.noreply.github.com>
2024-08-12 15:20:47 +0500

    XP: import react on all files

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import './App.css'
 import './assets/css/style.css'
 // import './assets/js/main.js'
 import Header from './components/Header.jsx'
-import Sidebar from './components/Sidebar.jsx'
 import Home from './pages/Home.jsx'
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
   return (
     <>
       <Header />
-      <Sidebar />
       <Home />
     </>
   )
